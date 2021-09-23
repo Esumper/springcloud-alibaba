fb340a3d38964456ecc71c29792c372c7561c3b5
layne <layne@atguigu.com>
2021-09-23 11:11:45 +0800

    Third commit

--- order/src/main/java/vip/sundany/controller/TestController.java
+++ order/src/main/java/vip/sundany/controller/TestController.java
@@ -3,5 +3,7 @@ package vip.sundany.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("123456");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
